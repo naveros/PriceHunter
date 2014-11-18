dd0ae58fe4543a85e56d294d098f19c7fee70a20
Frédéric Saucier-Naveros <fred_10540@hotmail.com>
2014-11-18 18:51:11 -0500

    CSS files added

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,19 +1,26 @@
 $('#searchButton').click(function(){
+
 	var input = document.getElementById("searchInput").value;
   $.ajax({url:"search",data:{key:input},success:function(result){
     
     var itemsDiv = document.getElementById("itemsDiv");
 
     var bestbuyItem = document.createElement("div");
-    bestbuyItem.class = "itemDiv";
+    bestbuyItem.class = "list-group-item";
 
-try {
+try { 
     var item = JSON.parse(result);
+
     var itemImage = document.createElement("img");
     itemImage.src = item.thumbnailImage ;
 
     var nom = document.createElement("span");
     nom.innerHTML = item.name;
+    nom.class= "list-group-heading";
+
+    var description = document.createElement("span");
+    description.innerHTML = item.shortDescription;
+    description.class= "item-text";
 
     var price = document.createElement("span");
     price.innerHTML = item.salePrice;
@@ -24,11 +31,16 @@ try {
 	siteRef.title = "Redirection vers bestbuy.com/"+item.name;
 	siteRef.href = item.url;
 
+
     bestbuyItem.appendChild(itemImage);
     bestbuyItem.appendChild(nom);
+    bestbuyItem.appendChild(description);
     bestbuyItem.appendChild(price);
     bestbuyItem.appendChild(siteRef);
+
     itemsDiv.appendChild(bestbuyItem);
+
+
 }catch(err) {
 	var error = document.createElement("span");
     error.innerHTML = "Désolé, aucun résultat pour cette recherche.";
@@ -36,4 +48,7 @@ try {
     itemsDiv.appendChild(error);
 }
   }});
+
+
+
 });
